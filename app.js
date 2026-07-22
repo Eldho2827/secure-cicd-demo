d3b39e9917d10b7dca8a12c877c917f16b7af13e
Eldho <eldhosabu8@gmail.com>
2026-07-22 13:05:50 +0530

    Add basic Node.js app with Dockerfile

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello from cicd-demo-app!'));
+app.get('/health', (req, res) => res.status(200).send('OK'));
+app.listen(3000, () => console.log('Listening on 3000'));
